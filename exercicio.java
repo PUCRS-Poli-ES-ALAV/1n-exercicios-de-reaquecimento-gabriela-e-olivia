04963ed5041baac1649b03607c41fd478110c674
gabrielaroxo <gabrielahroxo@gmail.com>
2025-02-26 22:05:07 -0300

    Descrição das alterações realizadas

--- exercicio.java
+++ exercicio.java
@@ -1,12 +1,30 @@
 public class exercicio {
 
     //Multiplicação de dois números naturais, através de somas sucessivas
+    public int multiplicacao(int a, int b){
+        if(a == 0) return 0;
+        return b + multiplicacao(a - 1,b);
+    }
 
     //Soma de dois números naturais, através de incrementos sucessivos
+    public int incrementoSucessivo(int a, int b){
+        if(b == 0) return 0;
+        return incrementoSucessivo(a + 1, b - 1);
+    }
+
 
     //Cálculo de 1 + 1/2 + 1/3 + 1/4 + ... + 1/N
+    public double calculo(int a){
+        if(a == 1) return 1.0;
+        return 1.0 / a + calculo(a - 1);
+    }
 
     //Inversão de uma string
+    public String inverter(String a){
+        if(a.isEmpty()) return "";
+        return inverter(a.substring(1)) + a.charAt(0);
+    }
+
 
     /*
     Gerador da sequência dada por:
@@ -14,6 +32,10 @@ public class exercicio {
     F(2) = 2
     F(n) = 2 ∗ F(n − 1) + 3 ∗ F(n − 2).
      */
+    public int fibonatti(int n){
+        if(n == 0) return 0;
+        return fibonatti(n - 1) + fibonatti(n - 2);
+    }
 
 
     /*
@@ -22,6 +44,7 @@ public class exercicio {
     A(m, n) = A(m − 1, 1), se m != 0 e n = 0
     A(m, n) = A(m − 1, A(m, n − 1)), se m != 0 e n != 0.
      */
+    public int sequenciaAckerman()
 
     //A partir de um vetor de números inteiros, calcule a soma e o produto dos elementos do vetor.
 
@@ -38,4 +61,9 @@ public class exercicio {
     fibg(f0, f1, 1) = f1
     fibg(f0, f1, n) = fibg(f0, f1, n − 1) + fibg(f0, f1, n − 2), se n > 1.
      */
+
+    public static void main(String[] args){
+
+    }
+
 }
